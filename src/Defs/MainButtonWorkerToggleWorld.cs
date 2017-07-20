fc2f555fbafae7b350442ad3d87e953ad0359e12
neitsa <neitsa@gmail.com>
2017-07-20 15:20:30 +0200

    Add new definition and its related class to be able to display the main window while playing (by clicking on the "World" menu in the menu bar).

---
+++ src/Defs/MainButtonWorkerToggleWorld.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using Verse;
+
+namespace PrepareLanding.Defs
+{
+
+    /// <summary>
+    /// This class is called from a definition file when clicking the "World" button on the bottom menu bar while playing
+    /// (see "PrepareLanding/Defs/Misc/MainButtonDefs/MainButtons.xml").
+    /// </summary>
+    public class MainButtonWorkerToggleWorld : MainButtonWorker_ToggleWorld
+    {
+        public override void Activate()
+        {
+
+            // default behavior
+            base.Activate();
+
+            // do not show the main window if in tutorial mode
+            if (TutorSystem.TutorialMode)
+            {
+                Log.Message(
+                    "[PrepareLanding] MainButtonWorkerToggleWorld: Tutorial mode detected: not showing main window.");
+                return;
+            }
+
+            // show the main window, minimized.
+            PrepareLanding.Instance.MainWindow.Show(true);
+        }
+    }
+}
