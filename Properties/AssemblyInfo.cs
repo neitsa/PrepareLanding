d13f52dfbbdbbeb62ef10b9dbe2a5e4ad87487e9
neitsa <neitsa@gmail.com>
2017-07-20 15:40:24 +0200

    Should be good for a 0.3 release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PrepareLanding")]
 [assembly: AssemblyDescription("A RimWorld Alpha 17 mod")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("neitsa")]
 [assembly: AssemblyProduct("PrepareLanding")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
