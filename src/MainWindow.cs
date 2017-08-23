1f7173322f97aeaa55f6c4680522152780a16009
neitsa <neitsa@gmail.com>
2017-08-23 23:03:20 +0200

    Set overlay & god mode tabs in debug mode only.

--- src/MainWindow.cs
+++ src/MainWindow.cs
@@ -52,9 +52,10 @@ namespace PrepareLanding
             _tabGuiUtilities.Add(new TabInfo(gameData, 0.48f));
             _tabGuiUtilities.Add(new TabOptions(gameData, 0.30f));
             _tabGuiUtilities.Add(new TabLoadSave(gameData, 0.48f));
+#if DEBUG
             _tabGuiUtilities.Add(new TabOverlays(gameData, 0.50f));
             _tabGuiUtilities.Add(new TabGodMode(gameData, 0.30f));
-
+#endif
             TabController.Clear();
             TabController.AddTabRange(_tabGuiUtilities);
 
