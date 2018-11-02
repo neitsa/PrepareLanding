1f4b27a9667baf71014aa861691a5a87ff0d1928
neitsa <neitsa@gmail.com>
2018-11-02 08:37:12 +0100

    Integrate Precise World Generation Percentage.

---
+++ src/Patches/CreateWorldParams.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+using Verse.Profile;
+using Verse.Sound;
+
+namespace PrepareLanding.Patches
+{
+    /// <summary>
+    ///     This class is used in replacement of the <see cref="RimWorld.Page_CreateWorldParams" /> class.
+    ///     We already have patched <see cref="RimWorld.PageUtility.StitchedPages" /> method (see
+    ///     <see cref="PageUtilityPatch.StitchedPagesPostFix" />) to use our own class rather than the RimWorld one.
+    /// </summary>
+    public class CreateWorldParams : Page
+    {
+        private readonly List<int> _fixedCoverages = new List<int>();
+
+        private bool _initialized;
+
+        private int _intCoverage;
+
+        private string _intCoverageString;
+
+        private float _planetCoverage;
+
+        private OverallRainfall _rainfall;
+
+        private string _seedString;
+
+        private OverallTemperature _temperature;
+
+        public override string PageTitle => "CreateWorld".Translate();
+
+        public override void PreOpen()
+        {
+            base.PreOpen();
+            if (!_initialized)
+            {
+                Reset();
+                _initialized = true;
+
+                _fixedCoverages.Clear();
+                _fixedCoverages.Add(5);
+
+                const int start = 10; // 10%
+                for (var cov = start; cov <= 100; cov += 10)
+                    _fixedCoverages.Add(cov);
+            }
+        }
+
+        public override void PostOpen()
+        {
+            base.PostOpen();
+            TutorSystem.Notify_Event("PageStart-CreateWorldParams");
+        }
+
+        public void Reset()
+        {
+            _seedString = GenText.RandomSeedString();
+            _planetCoverage = Prefs.DevMode && UnityData.isEditor ? 0.05f : 0.3f;
+            _intCoverage = Prefs.DevMode && UnityData.isEditor ? 5 : 30;
+            _rainfall = OverallRainfall.Normal;
+            _temperature = OverallTemperature.Normal;
+        }
+
+        public override void DoWindowContents(Rect rect)
+        {
+            DrawPageTitle(rect);
+            GUI.BeginGroup(GetMainRect(rect));
+            Text.Font = GameFont.Small;
+            var num = 0f;
+            Widgets.Label(new Rect(0f, num, 200f, 30f), "WorldSeed".Translate());
+            var rect2 = new Rect(200f, num, 200f, 30f);
+            _seedString = Widgets.TextField(rect2, _seedString);
+            num += 40f;
+            var rect3 = new Rect(200f, num, 200f, 30f);
+            if (Widgets.ButtonText(rect3, "RandomizeSeed".Translate()))
+            {
+                SoundDefOf.Tick_Tiny.PlayOneShotOnCamera();
+                _seedString = GenText.RandomSeedString();
+            }
+
+            num += 40f;
+            Text.Font = GameFont.Tiny;
+            var textAnchorBackup = Text.Anchor;
+            Text.Anchor = TextAnchor.MiddleCenter;
+            Widgets.Label(new Rect(0, num, rect3.xMax, 20f), "[Prepare Landing] Precise World Generation %");
+            Text.Font = GameFont.Small;
+            Text.Anchor = textAnchorBackup;
+
+            num += 20f;
+            Widgets.Label(new Rect(0f, num, 200f, 30f), $"{"PlanetCoverage".Translate()} [1,100]%");
+            var rect4 = new Rect(200f, num, 200f, 30f);
+
+            //
+            TextFieldNumericCoverage(rect4.LeftHalf());
+            ButtonCoverage(rect4.RightHalf());
+            //
+
+            TooltipHandler.TipRegion(new Rect(0f, num, rect4.xMax, rect4.height), "PlanetCoverageTip".Translate());
+            num += 40f;
+            Widgets.Label(new Rect(0f, num, 200f, 30f), "PlanetRainfall".Translate());
+            var rect5 = new Rect(200f, num, 200f, 30f);
+            _rainfall = (OverallRainfall) Mathf.RoundToInt(Widgets.HorizontalSlider(rect5, (float) _rainfall, 0f,
+                OverallRainfallUtility.EnumValuesCount - 1, true, "PlanetRainfall_Normal".Translate(),
+                "PlanetRainfall_Low".Translate(), "PlanetRainfall_High".Translate(), 1f));
+            num += 40f;
+            Widgets.Label(new Rect(0f, num, 200f, 30f), "PlanetTemperature".Translate());
+            var rect6 = new Rect(200f, num, 200f, 30f);
+            _temperature = (OverallTemperature) Mathf.RoundToInt(Widgets.HorizontalSlider(rect6, (float) _temperature,
+                0f, OverallTemperatureUtility.EnumValuesCount - 1, true, "PlanetTemperature_Normal".Translate(),
+                "PlanetTemperature_Low".Translate(), "PlanetTemperature_High".Translate(), 1f));
+            GUI.EndGroup();
+            DoBottomButtons(rect, "WorldGenerate".Translate(), "Reset".Translate(), Reset);
+        }
+
+        private void TextFieldNumericCoverage(Rect rect)
+        {
+            var tmpIntCoverage = _intCoverage;
+            _intCoverageString = tmpIntCoverage.ToString();
+
+            Widgets.TextFieldNumeric(rect, ref tmpIntCoverage, ref _intCoverageString, 1, 100);
+
+            if (tmpIntCoverage != _intCoverage)
+            {
+                _intCoverage = tmpIntCoverage;
+                _planetCoverage = _intCoverage / 100f;
+                if (_intCoverage >= 95)
+                    Messages.Message("MessageMaxPlanetCoveragePerformanceWarning".Translate(),
+                        MessageTypeDefOf.CautionInput);
+                if (_intCoverage <= 3)
+                    Messages.Message("Warning! Small planets may not offer settleable tiles",
+                        MessageTypeDefOf.CautionInput);
+            }
+        }
+
+        private void ButtonCoverage(Rect rect)
+        {
+            if (Widgets.ButtonText(rect, $"{_intCoverage}%"))
+            {
+                var floatMenuOptions = new List<FloatMenuOption>();
+
+                foreach (var fixedCoverage in _fixedCoverages)
+                {
+                    var text = $"{fixedCoverage}%";
+                    if (fixedCoverage <= 10)
+                        text += " (dev)";
+                    var coverage = fixedCoverage;
+                    var item = new FloatMenuOption(text, delegate
+                    {
+                        if (coverage != _intCoverage)
+                        {
+                            _intCoverage = coverage;
+                            _planetCoverage = _intCoverage / 100f;
+                            if (_intCoverage >= 95)
+                                Messages.Message("MessageMaxPlanetCoveragePerformanceWarning".Translate(),
+                                    MessageTypeDefOf.CautionInput);
+                        }
+                    });
+                    floatMenuOptions.Add(item);
+                }
+
+                Find.WindowStack.Add(new FloatMenu(floatMenuOptions));
+            }
+        }
+
+        protected override bool CanDoNext()
+        {
+            if (!base.CanDoNext()) return false;
+            LongEventHandler.QueueLongEvent(delegate
+            {
+                Find.GameInitData.ResetWorldRelatedMapInitData();
+                Current.Game.World =
+                    WorldGenerator.GenerateWorld(_planetCoverage, _seedString, _rainfall, _temperature);
+                LongEventHandler.ExecuteWhenFinished(delegate
+                {
+                    if (next != null) Find.WindowStack.Add(next);
+                    MemoryUtility.UnloadUnusedUnityAssets();
+                    Find.World.renderer.RegenerateAllLayersNow();
+                    Close();
+                });
+            }, "GeneratingWorld", true, null);
+            return false;
+        }
+    }
+}
--- src/Patches/PatchPageUtility.cs
+++ src/Patches/PatchPageUtility.cs
@@ -1,5 +1,8 @@
 ﻿using Harmony;
 using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace PrepareLanding.Patches
@@ -7,25 +10,39 @@ namespace PrepareLanding.Patches
     [HarmonyPatch(typeof(PageUtility), "StitchedPages")]
     public static class PageUtilityPatch
     {
-
         [HarmonyPostfix]
+        // ReSharper disable once InconsistentNaming
         public static void StitchedPagesPostFix(ref Page __result)
         {
-            Log.Message("[PrepareLanding] [postfix] RimWorld.PageUtility.StitchedPages");
+            Log.Message("[PrepareLanding] [postfix] StitchedPagesPostFix");
 
             if (__result == null)
             {
-                Log.Message("[PrepareLanding] [postfix] __result is null.");
+                Log.Message("[PrepareLanding] [postfix] StitchedPagesPostFix: __result is null.");
                 return;
             }
 
             if (TutorSystem.TutorialMode)
             {
-                Log.Message("[PrepareLanding] [postfix] Tutorial mode is ON.");
+                Log.Message("[PrepareLanding] [postfix] StitchedPagesPostFix: Tutorial mode is ON.");
                 return;
             }
 
-            var foundRequiredPage = false;
+            // patch for Page_SelectStartingSite to add our own button at the bottom of the screen.
+            var dictPageTypes = new Dictionary<Type, Page> { {typeof(Page_SelectStartingSite), new SelectLandingSite()} };
+            var dictFoundPagesTypes = new Dictionary<Type, bool>
+            {
+                {typeof(Page_SelectStartingSite), false},
+
+            };
+
+            // if precise world generation is not disabled, then we'll patch Page_CreateWorldParams with our own class.
+            if (!PrepareLanding.Instance.GameOptions.DisablePreciseWorldGenPercentage)
+            {
+                dictPageTypes.Add(typeof(Page_CreateWorldParams), new CreateWorldParams());
+                dictFoundPagesTypes.Add(typeof(Page_CreateWorldParams), false);
+            }
+
             var page = __result;
             while (true)
             {
@@ -33,35 +50,56 @@ namespace PrepareLanding.Patches
                 if (currentPage == null)
                     break;
 
-                if (currentPage.GetType() == typeof(Page_SelectStartingSite))
-                {
-                    Log.Message("[PrepareLanding] [postfix] Found Page_SelectLandingSite.");
-                    foundRequiredPage = true;
+                PatchPage(currentPage, dictPageTypes, dictFoundPagesTypes);
 
-                    // get next & previous of the original page
-                    var next = currentPage.next;
-                    var previous = currentPage.prev;
-
-                    var newPage = new SelectLandingSite();
-
-                    // unlinking!
-                    previous.next = newPage;
-                    next.prev = newPage;
-                    newPage.prev = previous;
-                    newPage.next = next;
-
-                    break;
-                }
                 page = currentPage;
             }
 
-            if (!foundRequiredPage)
+            if (!dictFoundPagesTypes.Values.All(v => v))
             {
-                Log.Error("[PrepareLanding] /!\\ Error !!!! Required page not found /!\\");
-                // TODO: disable the mod altogether?
+                Log.Error("[PrepareLanding] [postfix] StitchedPagesPostFix /!\\ Error !!!! Required page not found /!\\");
+                foreach (var foundPagesType in dictFoundPagesTypes)
+                {
+                    if(!foundPagesType.Value)
+                        Log.Error($"[PrepareLanding] [postfix] StitchedPagesPostFix; Missing page type: {foundPagesType.Key}");
+
+                }
             }
 
-            Log.Message("[PrepareLanding] [postfix] Done!");
+            Log.Message("[PrepareLanding] [postfix] StitchedPagesPostFix done!");
+        }
+
+        private static void PatchPage(Page currentPage, Dictionary<Type, Page> dictTypePages, IDictionary<Type, bool> dictFoundTypePages)
+        {
+            Type matchingType = null;
+            foreach (var key in dictTypePages.Keys)
+            {
+                if (currentPage.GetType() != key) continue;
+                matchingType = key;
+                break;
+            }
+
+            if (matchingType == null)
+                return;
+
+            var replacementPage = dictTypePages[matchingType];
+
+            Log.Message($"[PrepareLanding] [postfix] RimWorld.PageUtility.StitchedPages: Found {matchingType}.");
+
+            // get next & previous of the original page
+            var next = currentPage.next;
+            var previous = currentPage.prev;
+
+            // unlinking!
+            previous.next = replacementPage;
+            next.prev = replacementPage;
+            replacementPage.prev = previous;
+            replacementPage.next = next;
+
+            if (dictFoundTypePages.ContainsKey(matchingType))
+            {
+                dictFoundTypePages[matchingType] = true;
+            }
         }
     }
 }
